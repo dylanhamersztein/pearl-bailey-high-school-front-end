ddf2b06db2af0888ba2fff04d01a928a07f14361
Dylan Hamersztein <dylanhamersztein@users.noreply.github.com>
2022-10-31 01:02:52 +0000

    Rename default classes. Add bootstrap. Start creating nav bar component. Add GitHub Action file to build project. (#1)

---
+++ src/PearlBaileyHighSchoolFrontEnd.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PearlBaileyHighSchoolFrontEnd.scss';
+import {NavBar} from "./app/components/nav-bar/NavBar";
+
+function PearlBaileyHighSchoolFrontEnd() {
+    return (
+        <NavBar/>
+    );
+}
+
+export default PearlBaileyHighSchoolFrontEnd;
